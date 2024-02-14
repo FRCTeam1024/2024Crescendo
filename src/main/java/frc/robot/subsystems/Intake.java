3fe9530e208232cc92a4a85b4ffb20ba8773779b
1024Programming <team1024programming@gmail.com>
2024-02-13 21:02:52 -0500

    Add intake command

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import monologue.Logged;
@@ -24,5 +25,13 @@ public class Intake extends SubsystemBase implements Logged {
 
   public void stop() {
     setOutput(0);
+  }
+
+  public Command runIntakeCommand(double output) {
+    return runEnd(
+        () -> {
+          setOutput(output);
+        },
+        () -> stop());
   }
 }
