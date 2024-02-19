e4d1bfc377f340e25769bcc80ee79b12ebca61cc
1024Programming <team1024programming@gmail.com>
2024-02-19 17:56:37 -0500

    add superstructure setpoints

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.math.util.Units.*;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -43,5 +44,14 @@ public class Superstructure implements Logged {
   public record State(double armPosition, double wristPosition) {
     public static final State stow = new State(-0.5, 2.279);
     public static final State intake = new State(-0.5, 0);
+    public static final State scoreFromSubwoofer =
+        new State(degreesToRadians(-25), degreesToRadians(0));
+    public static final State scoreFromSpikeMark =
+        new State(degreesToRadians(-25), degreesToRadians(15));
+    public static final State scoreTrap = new State(degreesToRadians(60), degreesToRadians(10));
+    public static final State scoreAmp = new State(degreesToRadians(45), degreesToRadians(0));
+    public static final State scoreFromAmp = new State(degreesToRadians(-25), degreesToRadians(5));
+    public static final State scoreOverDefense =
+        new State(degreesToRadians(30), degreesToRadians(15));
   }
 }
